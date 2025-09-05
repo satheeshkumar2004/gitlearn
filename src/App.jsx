c3fd3c9921f61cf28eb1933f5e88dbf8ec7788a6
satheeshkumar2004 <21cs043@syedengg.ac.in>
2025-09-05 14:06:58 +0530

    new one

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+function ok(){
+
+  alert(document.getElementById("ok").value)
+}
+  return (
+    <>
+      <div>
+        <input id='ok' type='text' placeholder='enter the number'></input>
+        <button onClick={ok}>button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
